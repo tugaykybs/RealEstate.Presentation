0e5c4ac29f3e264d40e4316da0038db23f462a3a
tugay köybaşı <tugay_koybasi@hotmail.com>
2022-11-15 20:58:59 +0300

    proje devamı

--- RealEstate.Presantation/Startup.cs
+++ RealEstate.Presantation/Startup.cs
@@ -60,7 +60,7 @@ namespace RealEstate.Presantation
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseAuthentication();
             app.UseRouting();
 
             app.UseAuthorization();
@@ -70,6 +70,13 @@ namespace RealEstate.Presantation
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
